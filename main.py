bd87dddcbee74462631ef11e43c603baf2702463
Kevin Johnson <kmjohnson02@gmail.com>
2026-08-05 18:45:16 -0400

    created a main application entry point

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
-from pprint import pprint
-
-import data.load_data
-import data.transaction_data
-import data.auto_assign_rules
-import data.subcategory_definitions
-
-import classification.rule_based_assignment
 import classification.lookup_assignment
 import classification.manual_assignment
+import classification.rule_based_assignment
+
+import data.auto_assign_rules
+import data.load_data
+import data.subcategory_definitions
+import data.transaction_data
 
 import reports.print_transaction_pivot
 import reports.print_transaction_table
 
-transactions = data.load_data.load_raw_transactions(
-    data.transaction_data.raw_transactions
-)
 
-transactions = data.load_data.add_transaction_fields(
-    transactions
-)
+def main() -> None:
+    transactions = data.load_data.load_raw_transactions(
+        data.transaction_data.raw_transactions
+    )
 
-transactions = classification.rule_based_assignment.auto_assign_subcategories(
-    transactions,
-    data.auto_assign_rules.subcategory_assign_rules,
-)
+    transactions = data.load_data.add_transaction_fields(
+        transactions
+    )
 
-transactions = classification.manual_assignment.prompt_user_for_subcategory(
-    transactions,
-    data.subcategory_definitions.subcategories,
-)
+    transactions = (
+        classification.rule_based_assignment.auto_assign_subcategories(
+            transactions,
+            data.auto_assign_rules.subcategory_assign_rules,
+        )
+    )
 
-transactions = classification.lookup_assignment.apply_lookup_assignments(
-    transactions,
-    data.subcategory_definitions.subcategories,
-)
+    transactions = (
+        classification.manual_assignment.prompt_user_for_subcategory(
+            transactions,
+            data.subcategory_definitions.subcategories,
+        )
+    )
 
-reports.print_transaction_pivot.print_transaction_pivot(
-    transactions
-)
+    transactions = (
+        classification.lookup_assignment.apply_lookup_assignments(
+            transactions,
+            data.subcategory_definitions.subcategories,
+        )
+    )
 
-reports.print_transaction_table.print_transaction_table(
-    transactions
-)
+    reports.print_transaction_pivot.print_transaction_pivot(
+        transactions
+    )
 
-print()
-print("completed")
+    reports.print_transaction_table.print_transaction_table(
+        transactions
+    )
+
+
+if __name__ == "__main__":
+    main()
